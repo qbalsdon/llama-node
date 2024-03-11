a0787e7e1d2ad1ee0138ae0a2d5aedd8fc4cfb7e
Quintin Balsdon <quintinb@spotify.com>
2024-03-11 10:36:49 +0000

    Added 'npm install' to the setup

--- setup.sh
+++ setup.sh
@@ -10,12 +10,15 @@ nodeenv env
 NODE_VERSION=$(node -v)
 echo " --> Current node version is [$NODE_VERSION]. Recommended is at least [v21.7.0]"
 
+echo " --> Install npm dependencies"
+npm install
+
 echo " --> Creating model folder"
 mkdir -p models
 cd models
-echo " --> Downloading "
+echo " ├-> Downloading model"
 npx ipull https://huggingface.co/TheBloke/CapybaraHermes-2.5-Mistral-7B-GGUF/resolve/main/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf
 cd ..
 
-echo " --> Setup successful"
+echo " └-> Setup successful"
 deactivate_node
